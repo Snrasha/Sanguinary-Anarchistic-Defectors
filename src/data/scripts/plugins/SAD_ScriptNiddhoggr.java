57f4a294551fb106fcb544b3a44b96bfe1d0cd9e
Snrasha <snrasha@gmail.com>
2018-06-22 15:04:29 +0200

    update

---
+++ src/data/scripts/plugins/SAD_ScriptNiddhoggr.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.data.scripts.plugins;
+
+import com.fs.starfarer.api.combat.CombatEngineAPI;
+import com.fs.starfarer.api.combat.EveryFrameWeaponEffectPlugin;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipEngineControllerAPI;
+import com.fs.starfarer.api.combat.WeaponAPI;
+
+import java.util.Iterator;
+
+public class SAD_ScriptNiddhoggr implements EveryFrameWeaponEffectPlugin {
+
+    private float valuefra;
+
+
+    public SAD_ScriptNiddhoggr() {
+        valuefra = 0;
+
+    }
+
+    @Override
+    public void advance(float amount, CombatEngineAPI engine, WeaponAPI weapon) {
+        if (engine.isPaused()) {
+            return;
+        }
+
+        if (weapon == null || weapon.getShip() == null) {
+            return;
+        }
+
+        ShipAPI ship = weapon.getShip();
+
+        if (ship.isHulk() && !ship.isAlive()) {
+            return;
+        }
+
+        ShipEngineControllerAPI engin = ship.getEngineController();
+
+        boolean acc = engin.isAccelerating();
+        boolean dec = engin.isDecelerating();
+        boolean dacc = engin.isAcceleratingBackwards();
+
+        if ((dacc || dec) && valuefra <= 1f) {
+            valuefra += amount;
+            if (valuefra > 1f) {
+                valuefra = 1f;
+            }
+        } else if (acc) {
+            valuefra = 0.4f;
+        }
+
+        Iterator<ShipEngineControllerAPI.ShipEngineAPI> iter = engin.getShipEngines().iterator();
+        ShipEngineControllerAPI.ShipEngineAPI thruster;
+        while (iter.hasNext()) {
+            thruster = iter.next();
+            if (thruster.isActive() && thruster.getStyleId().startsWith("R")) {
+                if (dec) {
+                    engin.setFlameLevel(thruster.getEngineSlot(), valuefra);
+                } else if (acc) {
+                    engin.setFlameLevel(thruster.getEngineSlot(), 0.4f);
+                }
+                if (dacc) {
+                    engin.setFlameLevel(thruster.getEngineSlot(), valuefra);
+                }
+            }
+        }
+
+     
+    }
+
+}
